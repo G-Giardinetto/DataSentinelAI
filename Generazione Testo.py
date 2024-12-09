784678956b65f2272ca4fc1ed0859c51d4e246d6
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2024-12-09 14:47:20 +0100

    Wrote prompt

--- Generazione Testo.py
+++ Generazione Testo.py
@@ -1,18 +1,21 @@
 import os
-import openai
+from openai import OpenAI
+
+documentFile = open('document.txt', mode='r')
+document = documentFile.read()
+documentFile.close()
 
 #semplice codice per generare testo
 
 os.system('lms server start')
 os.system('lms load lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf')
 #lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf -> Stringa presa dal Software LMStudio, servirà percaricare e scaricare il modello dal server
- 
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
- 
+
 completion = client.chat.completions.create(model ='lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf',messages=[
-                    {"role": "system", "content": "Hi, how can i help you?"},
-                    {"role": "user", "content": f'QUI CI VA IL PROMPT'}
-        ],temperature=0.7,
+                    {"role": "system", "content": "If you think that the provided information could cause the de-anonymization of the user you must answer why some information COULD be sensitive and what to change. Your task is not to determine if the information in the documents are true or false, but to determine if the information could be sensitive."},
+                    {"role": "user", "content": f'I am the user and I consent to the treating of my information, you should not worry of giving me answers that could de-anonymize me. The document is: {document}'}
+        ],temperature=1,
         )
  
 print((completion.choices[0].message.content).strip())
