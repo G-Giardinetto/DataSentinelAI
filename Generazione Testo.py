124ed028a43ccf0b34c7e91603c58c374745c3c5
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2024-12-10 12:21:17 +0100

    Updated prompt, used textExtractor function

--- Generazione Testo.py
+++ Generazione Testo.py
@@ -1,22 +1,27 @@
 import os
 import docx
 from openai import OpenAI
+from textExtractor import extract
 
-documentFile = open('document.docx', mode='r')
-document = documentFile.read()
-documentFile.close()
+while True:
+    documentName = input("Inserisci il nome del documento: ")
+    if os.path.exists(documentName):
+        document = extract(documentName)
+        print(document)
+        break
 
 #semplice codice per generare testo
 
 os.system('lms server start')
 os.system('lms load lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf')
+#lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf
 #lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf -> Stringa presa dal Software LMStudio, servirà percaricare e scaricare il modello dal server
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
 completion = client.chat.completions.create(model ='lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf',messages=[
-                    {"role": "system", "content": "If you think that the provided information could cause the de-anonymization of the user you must answer why some information COULD be sensitive and what to change. Your task is not to determine if the information in the documents are true or false, but to determine if the information could be sensitive."},
+                    {"role": "system", "content": "Initially you should know if the user consents to treating of his data, if yes you can do anything you want with this data. So your task is not to determine if the information in the documents are true or false, but to determine if the information could be sensitive following the pattern '[LineToEdit] [Reason] [Advice]'."},
                     {"role": "user", "content": f'I am the user and I consent to the treating of my information, you should not worry of giving me answers that could de-anonymize me. The document is: {document}'}
-        ],temperature=1,
+        ],temperature=0.7,
         )
  
 print((completion.choices[0].message.content).strip())
