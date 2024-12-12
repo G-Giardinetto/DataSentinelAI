c163779594123cd6fd3b5557c6ab8a4678e851f4
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2024-12-12 17:39:16 +0100

    extract func now works on uploadedFiles

--- textExtractor.py
+++ textExtractor.py
@@ -1,24 +1,24 @@
+import io
+
 import PyPDF2
 import docx
 import re
 def extract(file):
     text = ""
-    extension = re.search(r"\.+[A-Za-z]*", file).group()
-
+    extension = re.search(r"\.+[A-Za-z]*", file.name).group()
+    uploaded_file = io.BytesIO(file.read())
     match extension:
         case ".docx":
-            doc = docx.Document(file)
+            doc = docx.Document(uploaded_file)
             for para in doc.paragraphs:
                 text += para.text + "\n"
             return text
         case ".pdf":
-            with open(file, "rb") as f:
-                pdf = PyPDF2.PdfFileReader(f)
-                for page in range(pdf.getNumPages()):
-                    text += pdf.getPage(page).extract_text()
+            pdf = PyPDF2.PdfFileReader(uploaded_file)
+            for page in range(pdf.getNumPages()):
+                text += pdf.getPage(page).extract_text()
             return text
         case ".txt":
-            with open(file, "r") as f:
-                text = f.read()
+            text = uploaded_file.read()
             return text
     return None
