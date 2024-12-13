74e8f1f9658a8c0c5380dfd94335e3619c2b3fce
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2024-12-13 11:41:13 +0100

    Update streamlitPage.py
    The page now shows loading icon during generation

--- streamlitPage.py
+++ streamlitPage.py
@@ -5,18 +5,17 @@ import matplotlib.pyplot as plt
 from textExtractor import extract
 import GenerazioneTesto
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
 st.title('Analyze your data')
 
 file = st.file_uploader("Upload your file to get you report", type=['pdf', 'docx', 'txt'], accept_multiple_files=False)
 
 if file is not None:
     testo = extract(file)
-    report = GenerazioneTesto.generate(testo)
+
     with st.chat_message("assistant"):
-        with st.status('Analizzando...'):
+        with st.status('Analizzando...') as status:
+            report = GenerazioneTesto.generate(testo)
+            status.update(
+                label="Analisi completa!", state="complete", expanded=True)
             st.write(f"Ecco il tuo report:\n{report}")
     # st.text(file._file_urls)
