ac059579b0b4343482a3c5bf075b90ac56d87ad3
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2024-12-30 17:27:16 +0100

    Added edit document functionality

--- streamlitPage.py
+++ streamlitPage.py
@@ -1,21 +1,36 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from textExtractor import extract
 import GenerazioneTesto
 
 st.title('Analyze your data')
 
 file = st.file_uploader("Upload your file to get you report", type=['pdf', 'docx', 'txt'], accept_multiple_files=False)
-
+testo=""
+flag=False
 if file is not None:
     testo = extract(file)
+    if st.button("Genera report", type="primary"):
+        flag=True
+        with st.chat_message("assistant"):
+            with st.status('Analizzando...') as status:
+                report = GenerazioneTesto.generateReport(testo)
+                status.update(
+                    label="Analisi completa!", state="complete", expanded=True)
+                st.markdown(f"Ecco il tuo report:\\ \n {report}")
+                st.session_state['report'] = report
 
-    with st.chat_message("assistant"):
-        with st.status('Analizzando...') as status:
-            report = GenerazioneTesto.generate(testo)
-            status.update(
-                label="Analisi completa!", state="complete", expanded=True)
-            st.write(f"Ecco il tuo report:\n{report}")
-    # st.text(file._file_urls)
+if flag:
+    st.write("Modificare il file secondo le indicazioni?")
+    button = st.button("Modifica", type="secondary", disabled=False)
+else:
+    button = st.button("Modifica", type="secondary", disabled=True, )
+
+if button:
+    report=st.session_state['report']
+    with st.status('Modificando...') as status:
+        left, right = st.columns(2)
+        edited = GenerazioneTesto.editDocument(testo, report)
+        status.update(
+            label="Modifica completata!", state="complete", expanded=True)
+        left.markdown(f"File modificato:\\ \n {edited}")
+        right.markdown(f"File originale:\\ \n {testo}")
