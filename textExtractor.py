1925d5ee48e9029ed53860c3ad3adb24c22bf5af
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2025-01-07 01:23:35 +0100

    created WriteFile function

--- textExtractor.py
+++ textExtractor.py
@@ -1,8 +1,9 @@
 import io
-
 import PyPDF2
 import docx
 import re
+from fpdf import FPDF
+
 def extract(file):
     text = ""
     extension = findExtension(file)
@@ -25,3 +26,21 @@ def extract(file):
 
 def findExtension(file):
     return re.search(r"\.+[A-Za-z]*", file.name).group()
+
+def writeFile(data, extension):
+    match extension:
+        case ".docx":
+            doc = docx.Document()
+            doc.add_paragraph(data)
+            doc.save("edited.docx")
+            return open("edited.docx", "rb").read()
+        case ".pdf":
+            pdf=FPDF()
+            pdf.add_page(orientation="P")
+            pdf.set_font("Arial", size=20)
+            pdf.multi_cell(w=0, h=20, txt=data, border=0, align="L")
+            pdf.output("edited.pdf")
+            return open("edited.pdf", "rb").read()
+        case ".txt":
+            return data
+    return None
