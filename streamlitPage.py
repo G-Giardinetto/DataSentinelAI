574f780054d9337e84e9574309bdc3b209ed1783
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2025-01-13 11:40:13 +0100

    Update streamlitPage.py

--- streamlitPage.py
+++ streamlitPage.py
@@ -3,40 +3,43 @@ from textExtractor import extract, writeFile
 from textExtractor import findExtension
 import GenerazioneTesto
 
+st.set_page_config(page_title="Analizza il tuo documento",layout="wide")
 st.title('Analyze your data')
-
-file = st.file_uploader("Upload your file to get you report", type=['pdf', 'docx', 'txt'], accept_multiple_files=False)
+first,center,last = st.columns([0.15,0.7,0.15])
+file = center.file_uploader("Upload your file to get you report", type=['pdf', 'docx', 'txt'], accept_multiple_files=False)
 testo=""
 flag=False
 if file is not None:
     st.session_state['fileExtension'] = findExtension(file)
     testo = extract(file)
-    if st.button("Genera report", type="primary"):
+    if center.button("Genera report", type="primary"):
         flag=True
-        with st.chat_message("assistant"):
-            with st.status('Analizzando...') as status:
+        with center.chat_message("assistant"):
+            with center.status('**Analizzando...**', expanded=True) as status:
                 report = GenerazioneTesto.generateReport(testo)
                 status.update(
                     label="Analisi completa!", state="complete", expanded=True)
-                st.markdown(f"Ecco il tuo report:\n\n {report}")
+                center.markdown(f"Ecco il tuo report:\n\n {report}")
                 st.session_state['report'] = report
 
 if flag:
-    st.write("Modificare il file secondo le indicazioni?")
-    button = st.button("Modifica", type="secondary", disabled=False)
+    center.write("Modificare il file secondo le indicazioni?")
+    button = center.button("Modifica", type="secondary", disabled=False, use_container_width=True)
 else:
-    button = st.button("Modifica", type="secondary", disabled=True)
+    button = center.button("Modifica", type="secondary", disabled=True, use_container_width=True)
 
 
 if button:
     report=st.session_state['report']
-    with st.status('Modificando...') as status:
-        left, right = st.columns(2)
+    with center.status('Modificando...') as status:
+        left, center, right = center.columns(3)
         edited = GenerazioneTesto.editDocument(testo, report)
+        nerEdited = GenerazioneTesto.extractEntities(edited)
         status.update(
             label="Modifica completata!", state="complete", expanded=True)
         left.markdown(f"File modificato:\n\n{edited}")
+        center.markdown(f"Entità riconosciute:\n\n{nerEdited}")
         right.markdown(f"File originale:\n\n{testo}")
     extension = st.session_state['fileExtension']
-    st.download_button(label="Scarica file modificato", data=writeFile(edited, extension),
+    center.download_button(label="Scarica file modificato", data=writeFile(edited, extension),
                        file_name='edited' + extension)
