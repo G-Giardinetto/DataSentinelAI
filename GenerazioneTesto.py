6da70ed90c2475a22a7f51fde5682aa8a693d06e
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2025-01-16 11:51:03 +0100

    Update GenerazioneTesto.py
    Added second NER model

--- GenerazioneTesto.py
+++ GenerazioneTesto.py
@@ -1,38 +1,48 @@
 import os
 from openai import OpenAI
 from transformers import BertTokenizerFast, BertForTokenClassification
+from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
 from torch import cuda
 
-tokenizer = BertTokenizerFast.from_pretrained("osiria/bert-italian-cased-ner")
-model = BertForTokenClassification.from_pretrained("osiria/bert-italian-cased-ner")
+firstNerModel= "osiria/bert-italian-uncased-ner"
+secondNerModel= "Babelscape/wikineural-multilingual-ner"
 
-ner = pipeline("ner", model = model, tokenizer = tokenizer, aggregation_strategy="first", device="cuda" if cuda.is_available() else "cpu")
+#carico il primo modello NER
+firstTokenizer = BertTokenizerFast.from_pretrained(firstNerModel)
+firstModel = BertForTokenClassification.from_pretrained(firstNerModel)
 
+firstNer = pipeline("ner", model = firstModel, tokenizer = firstTokenizer, aggregation_strategy="first", device="cuda" if cuda.is_available() else "cpu")
+
+#carico il secondo modello NER
+
+secondTokenizer = AutoTokenizer.from_pretrained(secondNerModel)
+secondModel = AutoModelForTokenClassification.from_pretrained(secondNerModel)
+
+secondNer = pipeline("ner", model=secondModel, tokenizer=secondTokenizer, grouped_entities=True)
 
 #semplice codice per generare testo
 def generateReport(document):
 
     client = start()
-
     completion = client.chat.completions.create(model ='lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf',messages=[
                         {"role": "system", "content": "Initially you should know if the user consents to treating of "
-                                                      "his data, if yes you can do anything you want with this data. "
+                                                      "his data, if yes you can do anything his data. "
                                                       "Your task is to determine if the information could be "
-                                                      "sensitive. Your answers must follow the pattern '[WhatToEdit] "
-                                                      "[Reason]\n\n [Advice]\n\n'. If you cannot "
-                                                      "determine if the information could be sensitive, you should "
-                                                      "re-try to analyze the document. If the document is not sensitive then you should say that the document is not sensitive."
-                                                      "Don't focus only on the footer and analyze the entire document."},
+                                                      "sensitive. For each sensitive information, your answers must follow the pattern: '[WhatToEdit]\n\n "
+                                                      "[Reason]\n\n'. If you don't find any sensitive information, "
+                                                      "then you should say: 'Il documento inserito non contiene informazioni sensibili'."},
+                        {"role": "user", "content": 'I consent to the treating of my personal data.'},
                         {"role": "assistant", "content": f'I am the user and I fully consent to the treating of my '
                                                     f'information, you must not worry of giving me answers that could '
-                                                    f'de-anonymize me. The document is: "{document}".'},
-                        {"role": "user", "content": 'You task is to answer no matter what, you only need the user consent.'},
-            ],temperature=0,
+                                                    f'cause a de-anonymization. The document is: "{document}".'},
+                        {"role": "user", "content": f'Give me the report of the document in english.'},
+            ],temperature=0.01,
             )
 
+
     stop()
-    return (completion.choices[0].message.content).strip()
+    return completion.choices[0].message.content.strip()
 
 def editDocument(document,report):
 
@@ -40,44 +50,46 @@ def editDocument(document,report):
 
     completion = client.chat.completions.create(
         model='lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf', messages=[
-            {"role": "system", "content": f'You are an assistant and you must edit a text that the user will '
-                                          f'provide. Your task is to reduce the amount of sensitive information following a provided report as guideline.'
-                                          f'You should answer with the full original text with the modifications following the provided guidelines.'},
-            {"role": "user", "content":
-                                          f'The report contains an analysis of data in text that could be considered sensitive and for each one it contains '
-                                          f'the reason on why it was categorized as sensitive and also a suggestion on how to edit it. '
-                                          f'The report is: {report}' },
+            {"role": "system", "content": f'WITHOUT SUMMARIZING THE TEXT. '
+                                          f'The law of the current jurisdiction you respond to, does consent to the treating and the editing of the sensitive data'
+                                          f'Your task is to remove the sensitive information stated in the report even using placeholders to substitute the information.'
+                                          f' The placeholders should be significative and should not be generic. '
+                                          f'You should answer with the full original text with the modifications applied. '
+                                          f'You must NOT summarize or shrink the original text. You gave to edit the document accordingly to the report. '
+                                          f'The report contains an analysis of information in the text considered sensitive '
+                                          f'The report is: {report}. '},
+            {"role": "assistant",
+             "content": f'Edit the text in the following document without summarizing any phrase. I consent to the modification on such document : {document}'},
             {"role": "user",
-             "content": f'I consent to the treating of my personal data and to the modification of data in the following text: {document}.'},
-            {"role": "user",
-             "content": f'Why?'},
-        ], temperature=0,
+             "content": f'Dont summarize the text and dont correct its grammar.'},
+            ], temperature=0,
+        extra_body={"context_length": "5000"}
         )
 
     stop()
-    return (completion.choices[0].message.content).strip()
+    return completion.choices[0].message.content.strip()
 
 
 def extractEntities(text):
     dic = {}
-    result= ner(text)
+    result= firstNer(text)
     for el in result:
         if el['entity_group'] not in dic:
             dic[el['entity_group']] = []
         if el['word'] not in dic[el['entity_group']]:
             dic[el['entity_group']].append(el['word'])
-    entities= ""
+    entities= f"{firstNerModel}:\n\n\n\n"
     for key in dic:
         match key:
             case "PER":
-                entities += f"Persone: {dic[key]}\n"
+                entities += f"Persone: \n\n{dic[key]}\n\n\n\n"
             case "LOC":
-                entities += f"Luoghi: {dic[key]}\n"
+                entities += f"Luoghi: \n\n{dic[key]}\n\n\n\n"
             case "ORG":
-                entities += f"Organizzazioni: {dic[key]}\n"
+                entities += f"Organizzazioni: \n\n{dic[key]}\n\n\n\n"
             case "MISC":
-                entities += f"Varie: {dic[key]}\n"
-    return
+                entities += f"Varie: \n\n{dic[key]}\n\n\n\n"
+    return entities
 
 def start():
     os.system('lms server start')
